df22f626a2549cc7b03596138334dc4c60b143c3
mstfmrt07 <60856457+mstfmrt07@users.noreply.github.com>
2020-12-27 06:05:20 +0300

    Help Manager has been added.

---
+++ PuraGameJam/Assets/Scripts/HelpManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class HelpItem
+    {
+        public string helpDescription;
+        public Sprite helpImage;
+    }
+
+    public List<HelpItem> helpItems = new List<HelpItem>();
+
+    public static HelpManager Instance {get; private set;}
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+}
--- PuraGameJam/Assets/Scripts/MenuController.cs
+++ PuraGameJam/Assets/Scripts/MenuController.cs
@@ -8,13 +8,13 @@ using System;
 public class MenuController : MonoBehaviour
 {
     public GameObject menuPanel, gameplayPanel, gameOverPanel, helpPanel, teamPanel, settingsPanel, gamePausedPanel, victoryPanel, levelStarPanel, levelPanel;
-    public RectTransform logo, buttonsPanel, levelSlidePanel;
-    public Button previousLevelButton, nextLevelButton, victoryNextLevelButton, startLevelButton, playButton;
-    public Image soundImage, musicImage, inGameSoundImage, inGameMusicImage, levelImage;
+    public RectTransform logo, buttonsPanel, levelSlidePanel, helpSlidePanel;
+    public Button previousLevelButton, nextLevelButton, previousHelpButton, nextHelpButton, victoryNextLevelButton, startLevelButton, playButton, helpButton;
+    public Image soundImage, musicImage, inGameSoundImage, inGameMusicImage, levelImage, helpItemImage;
     public Sprite toggleOn, toggleOff, grayButtonLongImage, greenButtonLongImage, lockIcon;
-    public Text levelNoText, levelNameText, birdCollectedCountText, fishCollectedCountText;
+    public Text levelNoText, levelNameText, helpDescriptionText, birdCollectedCountText, fishCollectedCountText;
 
-    private int currentlyDisplayedLevel = -1;
+    private int currentlyDisplayedLevel = -1, currentlyDisplayedHelpItem = -1;
     public static MenuController Instance;
 
     private void Awake()
@@ -29,6 +29,9 @@ public class MenuController : MonoBehaviour
 
         playButton.onClick.RemoveAllListeners();
         playButton.onClick.AddListener(() => DisplayLevel(PlayerPrefs.GetInt("CurrentLevel") - 1));
+
+        playButton.onClick.RemoveAllListeners();
+        helpButton.onClick.AddListener(() => DisplayHelpItem(0));
     }
 
     private void Start()
@@ -140,6 +143,61 @@ public class MenuController : MonoBehaviour
         {
             nextLevelButton.GetComponent<Image>().color = new Color(0.2666667f, 0.7490196f, 0.9764706f);
             nextLevelButton.interactable = true;
+        }
+    }
+
+    public void DisplayHelpItem(int index)
+    {
+        if (index >= 0 && index < HelpManager.Instance.helpItems.Count && currentlyDisplayedHelpItem != index)
+        {
+            if (currentlyDisplayedHelpItem < index && currentlyDisplayedHelpItem != -1) //goes right
+            {
+                helpSlidePanel.DOAnchorPosX(helpSlidePanel.anchoredPosition.x - 800f, 0.2f);
+                helpSlidePanel.DOAnchorPosX(helpSlidePanel.anchoredPosition.x + 1600f, 0).SetDelay(0.2f);
+                helpSlidePanel.DOAnchorPosX(0, 0.2f).SetDelay(0.21f);
+            }
+            else if (currentlyDisplayedHelpItem > index)
+            {
+                helpSlidePanel.DOAnchorPosX(helpSlidePanel.anchoredPosition.x + 800f, 0.2f);
+                helpSlidePanel.DOAnchorPosX(helpSlidePanel.anchoredPosition.x - 1600f, 0).SetDelay(0.2f);
+                helpSlidePanel.DOAnchorPosX(0, 0.2f).SetDelay(0.21f);
+            }
+
+            currentlyDisplayedHelpItem = index;
+            helpDescriptionText.text = HelpManager.Instance.helpItems[index].helpDescription;
+            helpItemImage.sprite = HelpManager.Instance.helpItems[index].helpImage;
+            AdjustHelpButtons(index);
+        }
+    }
+
+    public void AdjustHelpButtons(int index)
+    {
+        previousHelpButton.onClick.RemoveAllListeners();
+        nextHelpButton.onClick.RemoveAllListeners();
+
+        previousHelpButton.onClick.AddListener(() => DisplayHelpItem(index - 1));
+        nextHelpButton.onClick.AddListener(() => DisplayHelpItem(index + 1));
+
+        if (index == 0)
+        {
+            previousHelpButton.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f);
+            previousHelpButton.interactable = false;
+        }
+        else
+        {
+            previousHelpButton.GetComponent<Image>().color = new Color(0.2666667f, 0.7490196f, 0.9764706f);
+            previousHelpButton.interactable = true;
+        }
+
+        if (index == HelpManager.Instance.helpItems.Count - 1)
+        {
+            nextHelpButton.GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f);
+            nextHelpButton.interactable = false;
+        }
+        else
+        {
+            nextHelpButton.GetComponent<Image>().color = new Color(0.2666667f, 0.7490196f, 0.9764706f);
+            nextHelpButton.interactable = true;
         }
     }
 
